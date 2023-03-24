5b48ab8fb429a582e51ed105ccb09b9b19ec7e5f
chathura <cpriydasun@gmail.com>
2023-03-25 01:15:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function initMap() {
+    const royal = { lat: 48.874116215503925, lng: 2.3002677499125825 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: royal,
+    });
+    const marker = new google.maps.Marker({
+      position: royal,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+
+  
